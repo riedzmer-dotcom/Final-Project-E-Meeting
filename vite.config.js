db3411f97546870bf97edb776a448f2de54cb7ab
riedzmer <riedzmer@gmail.com>
2025-11-25 18:31:34 +0700

    Fix Vercel base path

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Konfigurasi Vite for React + Tailwind v4
 export default defineConfig({
-  base: "/Final-Project-E-Meeting/", // Name Repo
+  base: "/",   // FIX untuk Vercel — jangan pakai nama repo!
   plugins: [react()],
-  server: {
-    fs: {
-      strict: true, // path case-sensitive
-    },
-  },
-  optimizeDeps: {
-    force: true, // rebuild cache
-  },
 });
